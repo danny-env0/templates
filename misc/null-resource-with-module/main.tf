ef972cde6a926bb4bc429e1d4345b6b161047f02
danny-env0 <danny.sivan@env0.com>
2023-11-26 14:22:26 +0200

    Update main.tf

--- misc/null-resource-with-module/main.tf
+++ misc/null-resource-with-module/main.tf
@@ -1,6 +1,6 @@
 module "module" {
   source = "api-pr13934.dev.env0.com/f10d7b18-dc60-468c-9720-ea2389a17346/module/a"
-  version = "2.0.0"
+  version = "1.0.0"
 }
 
 resource "null_resource" "null" {
